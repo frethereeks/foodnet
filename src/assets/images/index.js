9ae78ced3382c4022e181d03d5ac37b31b7761c8
Frederick Jones <adefredy1@gmail.com>
2024-03-15 05:23:29 +0100

    Create images folder in the assets folder and export them for easy imports

---
+++ src/assets/images/index.js
@@ -0,0 +1,12 @@
+
+import ash from "./pexels-ash-376464.jpg" 
+import malidate from "./pexels-malidate-van-769289.jpg" 
+import robin from "./pexels-robin-stickel-70497.jpg" 
+import sydney from "./pexels-sydney-troxell-708488.jpg"
+
+export {
+    ash,
+    malidate,
+    robin,
+    sydney
+}
