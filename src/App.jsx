69c35cb071e23017d0681bd9f4f47df0ae9a7e09
Frederick Jones <adefredy1@gmail.com>
2024-03-15 05:28:27 +0100

    Set up the Pages path; making sure to create the Header and Footer components in the components folder and the PageLayout file in the layout folder for the react router dom outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Aos from "aos";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import PageLayout from "./layout/PageLayout";
+import Error from "./pages/Error";
+import Header from "./components/Header";
+import { useEffect } from "react";
+import SignUp from "./pages/SignUp";
+import Menu from "./pages/Menu";
+
 
 export default function App() {
+
+  useEffect(() => {
+    Aos.init({
+      duration: 2000,
+      once: true
+    })
+  },[])
+
+  const pageRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <PageLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/about",
+          element: <About />
+        },
+        {
+          path: "/menu",
+          element: <Menu />
+        },
+        {
+          path: "/login",
+          element: <Login />
+        },
+        {
+          path: "/signup",
+          element: <SignUp />
+        },
+      ],
+      errorElement: <>
+        <Header />
+        <Error />
+      </> 
+    },
+      
+    
+  ])
+
   return (
-    <div>App</div>
+      <div className="overflow-x-hidden">
+        <Toaster />
+        <RouterProvider router={pageRouter}></RouterProvider>
+      </div>
   )
 }
